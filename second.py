88e0d6eb47f652dc7499177484d17b0677f74290
webevis <xhanidholka3215@gmail.com>
2023-02-15 15:58:23 +0500

    second commit

--- second.py
+++ second.py
@@ -4,6 +4,10 @@ from flask import *
 import easyocr
 import re
 from datetime import datetime,date
+from deepface import DeepFace
+import base64
+from io import BytesIO
+from PIL import Image
 app = Flask(__name__)  
   
 @app.route('/')  
@@ -14,11 +18,28 @@ def main():
 def success():  
     if request.method == 'POST':  
         f = request.files['file']
-        f.save(f.filename)  
+        f2= request.files['fil']
+        
+
+        dat=f2.read()
+        my_string = base64.b64decode(dat)
+        imges=BytesIO(my_string)
+        img=Image.open(imges)
+        img.save("dp.png")
+        i= img.show()
+        print(i)
+     
+        data=f.read()
+        my_string = base64.b64decode(data)
+        imges=BytesIO(my_string)
+        img=Image.open(imges)
+        img.save("cnic.png")
+        i= img.show()
+        print(i)
+
         reader = easyocr.Reader(['en']) 
-        with open(f.filename, "rb") as f:
-          img = f.read()
-        result = reader.readtext(img,  detail = 0, paragraph=True)
+
+        result = reader.readtext("cnic.png",  detail = 0, paragraph=True)
         results="".join(result)
         
         CNIC_NO=re.findall(r'\d{5}[-]\d{7}[-]\d{1}', results)[0]
@@ -52,23 +73,34 @@ def success():
             EXPIRE="your card is expire"
         else:
             EXPIRE= "their are "+ str(EXPIRE.days)+" days remaining to expire card"
+            
+            F_NAME=  re.findall("[A-Za-z]+\s?", results)
+            F_NAME=''.join(F_NAME)
 
-        NAMES=  re.findall("[e]\s[A-Z][a-z]+\s[A-Z][a-z]+\S", results)[0]
-        FIRST_NAMES=  re.findall("[A-Z][a-z]+\S", NAMES)[0]
-        LAST_NAMES=  re.findall("[A-Z][a-z]+\S", NAMES)[1]
+            NAMES=  re.findall("[A-Za-z]{3}[e]\s[A-Z][a-z]+\s[A-Z][a-z]+\S", F_NAME)[0]
+            FIRST_NAMES=  re.findall("[A-Z][a-z]+\S", NAMES)[1]
+            LAST_NAMES=  re.findall("[A-Z][a-z]+\S", NAMES)[2]
 
-        F_NAME=  re.findall("[e]\s[A-Z][a-z]+\s[A-Z][a-z]+\S", results)[1]
-        FIRST_NAME=  re.findall("[A-Z][a-z]+\S", F_NAME)[0]
-        LAST_NAME=  re.findall("[A-Z][a-z]+\S", F_NAME)[1]
-        if LAST_NAME==re.findall("[A-Z][a-z]+[L]", LAST_NAME)[0]:
-         LAST_NAME= LAST_NAME[::-1]
-         LAST_NAME= LAST_NAME.replace("L", '',1)
-         LAST_NAME= LAST_NAME[::-1]
+            Father_name=re.findall("[A-Za-z]{3}[e]\s[A-Z][a-z]+\s[A-Z][a-z]+\S", F_NAME)[1]
+            FIRST_NAME=  re.findall("[A-Z][a-z]+\S", Father_name)[1]
+            LAST_NAME=  re.findall("[A-Z][a-z]+\S", Father_name)[2]
+            
+            
+           
+            """ if LAST_NAME==re.findall("[A-Z][a-z]+[L]", LAST_NAME)[0]:
+             LAST_NAME= LAST_NAME[::-1]
+             LAST_NAME= LAST_NAME.replace("L", '',1)
+             LAST_NAME= LAST_NAME[::-1]"""
+           
+            result = DeepFace.verify(img1_path = "cnic.png", img2_path = "dp.png")
+            #if result['verified']==True:
+              #  result= "its same person"
+        
+        
 
         #a=type(result)
-        my_information = {'name': {'FIRST_NAME':FIRST_NAMES, 'LAST_NAME':LAST_NAMES }, 'cnic no.': CNIC_NO, 'Father name':  {'FIRST_NAME':FIRST_NAME, 'LAST_NAME':LAST_NAME }, 'Date of Birth': DATE_OF_BIRTH, 'Date of issue': DATE_OF_ISSUE, 'Date of expire': DATE_OF_EXPIRE, 'Today': TODAY, 'expire':EXPIRE, 'age': age }
-        return render_template("response.html", name = my_information) 
+        my_information ={'name':{'first-name': FIRST_NAMES,'last-name':LAST_NAMES},'Father-name':{'first-name': FIRST_NAME,'last-name':LAST_NAME}, 'cnic no.': CNIC_NO, 'Date of Birth': DATE_OF_BIRTH, 'Date of issue': DATE_OF_ISSUE, 'Date of expire': DATE_OF_EXPIRE, 'Today': TODAY, 'expire':EXPIRE, 'age': age, 'results':result }
+        return render_template("response.html", name =  my_information) 
        
-  
 if __name__ == '__main__':  
     app.run(debug=True) 
